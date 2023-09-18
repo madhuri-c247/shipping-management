37c175362e7a327bc271d43375b3166a58afb8a3
madhuri-c247 <madhuri.v@247madhuri.v@chapter247.com>
2023-09-18 17:31:32 +0530

    home--

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { NavLink, Outlet } from "react-router-dom";
+//css
+import styles from "./home.module.scss";
+//assets
+import background from "../../assets/homeBackground.jpg";
+//react-icons
+import { BsEnvelope } from "react-icons/bs";
+import { LuPackage2 } from "react-icons/lu";
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div
+      className={`${styles.container}`}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className={`${styles.overlay}`}></div>
+      <div className={`${styles.shipmentContainer}`}>
+        <div className={`${styles.heading}`}>
+          <h1>Book a shipment instantly</h1>
+          <pre>
+            The fast and easy shipping solution for your business. Get your
+            quote in seconds today
+          </pre>
+        </div>
 
-export default Home
+        <div className={`${styles.shipmentForm}`}>
+          <ul>
+            <NavLink to={"/home/letterSelection"}>
+              <BsEnvelope className={`${styles.icon}`} />
+              Letter
+            </NavLink>
+            <NavLink to={"/home/packageSelection"}>
+              <LuPackage2 className={`${styles.icon}`} />
+              Package
+            </NavLink>
+          </ul>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
